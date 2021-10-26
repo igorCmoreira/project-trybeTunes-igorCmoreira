b7220ff80fdaf7c7bc210a3d6ce7dd5385da0f97
Igor Campos Moreira <igorcampos200@gmail.com>
2021-10-26 18:23:15 -0300

    Requisito 1

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Album from './pages/Album';
+import Favorites from './pages/Favorites';
+import Login from './pages/Login';
+import NotFound from './pages/NotFound';
+import Profile from './pages/Profile';
+import ProfileEdit from './pages/ProfileEdit';
+import Search from './pages/Search';
 
 class App extends React.Component {
   render() {
-    return (<p>TrybeTunes!!</p>);
+    return (
+      <BrowserRouter>
+        <Switch>
+          {/* <Route component={ NotFound } />
+          => como o switch do react é comparavel com o do js colocarei,
+           esta linha na utima posição */ }
+          <Route exact path="/" component={ Login } />
+          <Route path="/Album" component={ Album } />
+          <Route path="/Favorites" component={ Favorites } />
+          <Route path="/NotFound" component={ NotFound } />
+          <Route exact path="/Profile" component={ Profile } />
+          <Route exact path="/profile/edit" component={ ProfileEdit } />
+          <Route path="/Search" component={ Search } />
+          <Route component={ NotFound } />
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
