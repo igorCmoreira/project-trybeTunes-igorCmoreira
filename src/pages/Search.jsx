67af0e7bb9123c0ecfbc06016146950851b26ab7
Igor Campos Moreira <igorcampos200@gmail.com>
2021-10-28 02:17:24 -0300

    Requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,10 +2,44 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      music: '',
+      enableBu: true,
+    };
+  }
+
+  handleChange = (event) => {
+    const { value } = event.target;
+    const N = 2;
+    if (value.length >= N) {
+      this.setState({ enableBu: false });
+    } else {
+      this.setState({ enableBu: true });
+    }
+    this.setState({ music: value });
+  }
+
   render() {
+    const { enableBu } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
+        <section>
+          <input
+            type="text"
+            onChange={ this.handleChange }
+            data-testid="search-artist-input"
+          />
+          <button
+            type="submit"
+            data-testid="search-artist-button"
+            disabled={ enableBu }
+          >
+            Pesquisar
+          </button>
+        </section>
       </div>
     );
   }
